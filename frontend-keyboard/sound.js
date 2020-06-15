cb7ee096738073b3279efdd67eb7fa0e5aaf8f17
github username <github email address>
2020-06-15 16:42:46 -0500

    Start of front end

---
+++ frontend-keyboard/sound.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body")
+
+
+const adddiv = document.createElement("div")
+adddiv.setAttribute("class","grid-container")
+adddiv.innerText = "Testing"
+body.append(adddiv)
+
